2cefd03e15c198eb7636090c9b7fe0674315aa0f
Mike Earley <mwe@sent.com>
2026-07-26 11:26:37 -0400

    removed redux, added tanstack query; implemented removeDeal and updateDeal to allow for deleting and publishing and unpublishing deals. Added new actions column, and added buttons for performing functionality. Functionality works in smoke testing. Still dealing with issues related to json-server id assignment and conversion from numbers to strings.

--- src/features/DealsTable/DealsTableRow/DealsTableRow.tsx
+++ src/features/DealsTable/DealsTableRow/DealsTableRow.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { DealType } from "../../../types";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { removeDeal, updateDeal } from "../fetch";
 
 import "./DealsTableRow.scss";
 
@@ -13,16 +15,46 @@ type DealsTableRowProps = {
 
 const DealsTableRow = (props: DealsTableRowProps) => {
   const {
-    deal: { institution, dealType, dealSize, isPublished },
+    deal: { id, institution, dealType, dealSize, isPublished },
   } = props;
+
+  const queryClient = useQueryClient();
+
+  const { mutate: remove, isPending: isRemoving } = useMutation({
+    mutationFn: () => removeDeal(id!),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["deals"] }),
+  });
+
+  const { mutate: togglePublish, isPending: isToggling } = useMutation({
+    mutationFn: () => updateDeal(id!, { isPublished: !isPublished }),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["deals"] }),
+  });
+
   return (
-    <tr className='DealsTableRow'>
-      <td className='DealsTableRow--cell'>{institution}</td>
-      <td className='DealsTableRow--cell'>{dealType}</td>
-      <td className='DealsTableRow--cell'>
+    <tr className="DealsTableRow">
+      <td className="DealsTableRow--cell">{institution}</td>
+      <td className="DealsTableRow--cell">{dealType}</td>
+      <td className="DealsTableRow--cell">
         {currencyAmountToString(dealSize)}
       </td>
-      <td className='DealsTableRow--cell'>{isPublished ? "Yes" : "No"}</td>
+      <td className="DealsTableRow--cell">{isPublished ? "Yes" : "No"}</td>
+      <td className="DealsTableRow--cell flex-row">
+        <button
+          className="button--transparent"
+          onClick={() => remove()}
+          disabled={isRemoving}
+        >
+          Delete
+        </button>
+        <span>|</span>
+        <button
+          className="button--transparent"
+          onClick={() => togglePublish()}
+          disabled={isToggling}
+        >
+          {isPublished ? "Unpublish" : "Publish"}
+        </button>
+      </td>
     </tr>
   );
 };
